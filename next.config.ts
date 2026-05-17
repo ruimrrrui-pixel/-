726362b2f7ea5ac05c85b330cefe8f511bfa07d3
田川晶悠 <akiharutagawa@tagawaakirayuunoMacBook-Air.local>
2026-05-17 16:43:29 +0900

    ビルドエラー修正

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  devIndicators: false,
 }
 
 export default nextConfig
